1d4ec86d742fef6b81b5676d34c6a67ce9a1177c
Moses Ma <701000372@GoTVDSB.ca>
2025-10-07 09:34:15 -0400

    Pretty sure im done

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
-﻿namespace _1_7_Summative
+﻿using System.Collections.Generic;
+
+namespace _1_7_Summative
 {
     internal class Program
     {
         static int money = 100;
+
+        static int rpsbot;
+
+        static int bet = 0;
         static int BetLoop()
         {
             int Bet = 0;
@@ -12,21 +18,20 @@
                 if (input == "all")
                 {
                     Bet = money;
-                    break;
                 }
-                if (int.TryParse(input, out Bet))
+                int.TryParse(input, out Bet);
+                if (Bet <= 0 || Bet > money)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid input, try to give a positive input or an input that is not over your current balance.");
+                    Console.WriteLine($"How much do you want to bet, you currently own {money.ToString("c")} (Enter numbers, ex. 100)");
+                }
+                else
                 {
                     break;
                 }
-                Console.Clear();
-                Console.WriteLine("Invalid input, try again (I don't take cents)");
-                Console.WriteLine($"How much do you want to bet, you currently own {money.ToString("c")} (Enter numbers, ex. 100)");
             }
-            if (Bet <= 0 || Bet > money)
-            {
-                Console.Clear();
-                Console.WriteLine("Invalid input, try to give a positive input or an input that is not over your current balance.");
-            }
+            
             return Bet;
         }
         static string rps;
@@ -54,35 +59,125 @@
                 Console.WriteLine("Invalid input, just enter rock paper or scissor bruh...it ain't that hard");
             }
         }
+        static void rpsoutput()
+        {
+            if (rpsbot == 0)
+            {
+                if (rps == "r") 
+                {
+                    Console.WriteLine("The computer picked rock, you tied");
+                }
+                else if (rps == "p")
+                {
+                    Console.WriteLine("The computer picked paper, you tied");
+                }
+                else if (rps == "s")
+                {
+                    Console.WriteLine("The computer picked scissors, you tied");
+                }
+            }
+            else if (rpsbot == 1)
+            {
+                if (rps == "r")
+                {
+                    Console.WriteLine("The computer picked scissor, you won");
+                }
+                else if (rps == "p")
+                {
+                    Console.WriteLine("The computer picked rock, you won");
+                }
+                else if (rps == "s")
+                {
+                    Console.WriteLine("The computer picked paper, you won");
+                }
+                money = money + bet;
+            }
+            else if (rpsbot == 2)
+            {
+                if (rps == "r")
+                {
+                    Console.WriteLine("The computer picked paper, you lost");
+                }
+                else if (rps == "p")
+                {
+                    Console.WriteLine("The computer picked scissor, you lost");
+                }
+                else if (rps == "s")
+                {
+                    Console.WriteLine("The computer picked rock, you lost");
+                }
+                money = money - bet;
+            }
+        }
+        static void moneygain()
+        {
+            int gain;
+            gain = money - 100;
+            if (gain < 0)
+            {
+                Console.WriteLine("Too Bad Too Sad, you lost " + Math.Abs(gain).ToString("C"));
+            }
+            else if (gain > 0)
+            {
+                Console.WriteLine("Nice job, you won " + gain.ToString("c"));
+            }
+            else if (gain == 0)
+            {
+                Console.WriteLine("Atleast you didn't lose money...");
+            }
+        }
         static void Main(string[] args)
         {
-            int bet = 0;
+            Random generator;
+            generator = new Random();
+
+            
             bool done = false;
 
             string project = "";
 
+            bool menu = false;
+
             while (!done)
             {
-                Console.WriteLine(@"
+                if (menu == false)
+                {
+                    Console.WriteLine(@"
 Welcome to my Rock Paper Scissor game, this game is literally Rock Paper Scissor, just with a little gambling element in it.
 RULE | NO CENTS ALLOWED
 1. Rock Paper Scissor
 2. Quit
+3. Rules
 ");
-                project = Console.ReadLine();
+                    menu = true;
+                }
+                else
+                {
+                    Console.WriteLine("1. Rock Paper Scissor\r\n2. Quit\r\n3. Rules");
+                }
+                    project = Console.ReadLine();
 
                 if (project == "1" && money != 0)
                 {
-                    Console.WriteLine("Enter the amount of money you want to bet, you currently have " + money);
+                    Console.WriteLine("Enter the amount of money you want to bet, you currently have " + money.ToString("c"));
                     bet = BetLoop();
                     Console.WriteLine("Enter your input, Rock(r), Paper(p), Scissor(s)");
                     RPSloop();
-
+                    rpsbot = generator.Next(3);
+                    rpsoutput();
+                    Console.WriteLine("Press ENTER to continue");
+                    Console.ReadLine();
+                    Console.Clear();
                 }
                 else if (project == "2")
                 {
-                    Console.WriteLine("Thank you for playing, never see you again.");
+                    moneygain();
                     done = true;
+                }
+                else if (project == "3") 
+                {
+                    Console.Clear ();
+                    Console.WriteLine("THE ONLY RULE IS ONLY INPUT INTEGERSSSS, because my system is not too intelligented.\n By the way, the game work by just you inputting your bet and your choice and then I will tell you how much I scam you for.");
                 }
                 else if (money == 0)
                 {
